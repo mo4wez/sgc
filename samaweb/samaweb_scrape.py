5ac3bd729980e1e7334f5d1813218eb5c6e69595
moawezz <moawez.de@gmail.com>
2024-07-01 21:45:41 +0330

    add log-level-argument

--- samaweb/samaweb_scrape.py
+++ samaweb/samaweb_scrape.py
@@ -31,11 +31,11 @@ class SamaGradeChecker:
 
         logging.info('Setting up driver...')
         options = Options()
-        options.add_argument(f"--no-proxy-server={self.config.login_url}")
-        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--headless")
         options.add_argument("--disable-gpu")
+        options.add_argument("--log-level=3")
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
         return driver
